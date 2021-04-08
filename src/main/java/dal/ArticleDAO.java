a5d5d9b52de9d665766d41d320540ac756379f2f
Michael <michael.mery@hotmail.fr>
2021-04-08 12:25:05 +0200

    Article MM

--- src/main/java/dal/ArticleDAO.java
+++ src/main/java/dal/ArticleDAO.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 public interface ArticleDAO {
 
-    public void insert(Article nouvelArticle) throws DALException;
+    public Article insert(Article nouvelArticle) throws DALException;
 
     public List<Article> selectAll() throws DALException ;
 
     public Article selectById(int id) throws DALException ;
 
     public void delete(int id) throws DALException ;
+
+    void update(Article art) throws  DALException;
 }
